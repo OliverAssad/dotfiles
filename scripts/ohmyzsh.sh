4323fb8ddf20b3e0cb291528a01c8c9f11b08b73
OliverAssad <oliver.assad@nordcloud.com>
2023-06-09 13:31:29 +0000

    revert

--- scripts/ohmyzsh.sh
+++ scripts/ohmyzsh.sh
@@ -11,16 +11,16 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 # sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc
 
 # # Install zsh-autosuggestions plugin
-# git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
 # # Install zsh-syntax-highlighting plugin
-# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 # # Set theme to "minimal"
-# sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="minimal"/g' ~/.zshrc
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="minimal"/g' ~/.zshrc
 
 # # Activate plugins
-# sed -i 's/# source $ZSH\/oh-my-zsh.sh/source $ZSH\/custom\/plugins\/zsh-autosuggestions\/zsh-autosuggestions.zsh\nsource $ZSH\/custom\/plugins\/zsh-syntax-highlighting\/zsh-syntax-highlighting.zsh\nsource $ZSH\/oh-my-zsh.sh/g' ~/.zshrc
+sed -i 's/# source $ZSH\/oh-my-zsh.sh/source $ZSH\/custom\/plugins\/zsh-autosuggestions\/zsh-autosuggestions.zsh\nsource $ZSH\/custom\/plugins\/zsh-syntax-highlighting\/zsh-syntax-highlighting.zsh\nsource $ZSH\/oh-my-zsh.sh/g' ~/.zshrc
 
 # # Restart Zsh
 # exec zsh
